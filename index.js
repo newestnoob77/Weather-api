1fab91aae7cbd6b35a1c47d139e4e94f8b831831
newestnoob77 <gsjithin1234@gmail.com>
2026-03-08 16:49:39 +0000

    redis configured

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import { client } from './src/config/redisClient.js'
 const app =express()
-app.get("/",(req,res,next)=>res.status(200).send("Server is listening"))
+app.get("/test-cache",async(req,res)=>{
+    await client.set("name","jithu");
+    const value =await client.get("name");
+    res.json({cachedValue:value})
+})
 app.listen(3000,(req,res,next)=>{
 console.log("Server is listening")
 })
--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,1 +1,8 @@
 import redis from 'redis'
+export const client =redis.createClient({
+    url: "redis://localhost:6379"
+});
+client.connect()
+client.on("error",(err)=>{
+    console.log("Redis Error",err);
+})
