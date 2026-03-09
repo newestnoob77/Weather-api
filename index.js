f64e0107b17e48d128b1aa969cbfd9bb362163e0
newestnoob77 <gsjithin1234@gmail.com>
2026-03-09 09:49:39 +0000

    Error handiling,rate limiting

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import "./env.js";
 import express from 'express'
 import { weatherRouter } from "./src/weather/weatherRouter.js";
+import { limiter } from "./src/middleware/rateLimiter.middleware.js";
+import { ApplicationError } from "./src/middleware/ApplicationError.js";
 const app =express();
+app.set('trust proxy', 1);
+app.use(limiter)
 app.use("/api",weatherRouter)
 
-app.listen(3000,(req,res,next)=>{
+app.use((err,req,res,next)=>{
+    if(err instanceof ApplicationError){
+        return res.status(err.code).send(err.message)
+    }
+    res.status(500).send("Soemthing went wrong");
+
+})
+app.listen(3000,()=>{
 console.log("Server is listening")
 })
